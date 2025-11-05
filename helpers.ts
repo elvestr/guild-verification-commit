25f79ab5753d3ff3d2cac51f0e9d17363deb7f34
ren ken <36890620+elvestr@users.noreply.github.com>
2025-11-06 02:03:55 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:03:50.434Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-11-05T23:03:54.483Z
+Commit: Patch API endpoint error handling
