fcefdccb4e09576b77aa8ecac77bceaa1a502cc1
ren ken <36890620+elvestr@users.noreply.github.com>
2025-11-05 22:39:47 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:39:32.191Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-05T19:39:45.655Z
+Commit: Patch security vulnerability in authentication
