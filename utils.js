c25b3dfd24dbd067ca775833b2ef1a1ddc4284f1
ren ken <36890620+elvestr@users.noreply.github.com>
2025-11-06 01:58:46 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T22:58:34.250Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-05T22:58:45.054Z
+Commit: Fix race condition in async operations
