bfc3a0aef8992873874bbb057fa27a95e7fe17dc
ren ken <36890620+elvestr@users.noreply.github.com>
2025-11-05 22:44:10 +0300

    Introduce real-time notifications

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:44:05.487Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-05T19:44:08.207Z
+Commit: Introduce real-time notifications
