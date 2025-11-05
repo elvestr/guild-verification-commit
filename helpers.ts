d54170814f9b4a260b3f584f9a99e590025f7fd7
ren ken <36890620+elvestr@users.noreply.github.com>
2025-11-05 22:39:34 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:39:28.048Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-05T19:39:32.191Z
+Commit: Resolve null pointer exception in user service
