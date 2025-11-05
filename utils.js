f10ad5eb37c4ac46b0b6e2b719d1c9db316041f6
ren ken <36890620+elvestr@users.noreply.github.com>
2025-11-06 02:03:32 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:02:39.227Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-05T23:03:31.428Z
+Commit: Resolve timezone issues in date handling
