0b7ea84486cabfd3618eddad6d553a7d42064fe1
ren ken <36890620+elvestr@users.noreply.github.com>
2025-11-06 01:57:54 +0300

    Add multi-language support

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T22:57:29.244Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-05T22:57:52.469Z
+Commit: Add multi-language support
