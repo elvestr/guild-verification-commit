aec00bff97ec091582af0f866bfa8155fc1080b7
ren ken <36890620+elvestr@users.noreply.github.com>
2025-11-06 02:00:27 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T22:57:54.320Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-05T23:00:26.296Z
+Commit: Fix memory leak in event handlers
