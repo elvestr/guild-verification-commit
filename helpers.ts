c752262c63cb3cf1903232a7dac579a28ac8875b
ren ken <36890620+elvestr@users.noreply.github.com>
2025-11-05 22:42:53 +0300

    Fix database connection timeout

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:39:45.655Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-11-05T19:42:51.415Z
+Commit: Fix database connection timeout
