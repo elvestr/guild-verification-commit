fa2a648c0b3cc0c31d10864e063e903d665ca420
ren ken <36890620+elvestr@users.noreply.github.com>
2025-11-06 01:51:28 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T22:51:10.325Z
-Commit: Fix race condition in async operations
+Timestamp: 2025-11-05T22:51:26.974Z
+Commit: Resolve null pointer exception in user service
