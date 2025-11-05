b2885cced565e2218cf9f97a823d8461fb955f9f
ren ken <36890620+elvestr@users.noreply.github.com>
2025-11-06 01:50:56 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:44:10.961Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-05T22:50:55.144Z
+Commit: Resolve null pointer exception in user service
