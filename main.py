6460a5312c9c729fb254e00c2290128b231ca51e
ren ken <36890620+elvestr@users.noreply.github.com>
2025-11-06 02:00:37 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:00:31.010Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-05T23:00:36.500Z
+Commit: Fix broken links in documentation
