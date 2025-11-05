ffdb82b5a957a0850df2b35ce8c2aad310f8cc87
ren ken <36890620+elvestr@users.noreply.github.com>
2025-11-06 02:03:46 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:03:34.064Z
-Commit: Add export functionality for reports
+Timestamp: 2025-11-05T23:03:45.228Z
+Commit: Fix CSS styling bug on mobile devices
