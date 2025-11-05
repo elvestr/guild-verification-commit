816aa8ee059119a3bfda21b457a4e87d1bf810c1
ren ken <36890620+elvestr@users.noreply.github.com>
2025-11-06 01:51:29 +0300

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T22:51:24.423Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-05T22:51:28.206Z
+Commit: Add multi-language support
